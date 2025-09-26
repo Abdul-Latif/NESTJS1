cd5d34a1e5ed9790115d216f1250d81615527f54
Abdullatif <m3yiel@gmail.com>
2025-09-27 00:01:12 +0300

    new

--- src/payment/dto/create-payment.dto.ts
+++ src/payment/dto/create-payment.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+
 export class CreatePaymentDto {
+    @IsNumber()
+    @Max(20000)
+    @Min(0)
     amount: number;
+
+    @IsString()
+    @IsNotEmpty()
+
     method: string;
 }
